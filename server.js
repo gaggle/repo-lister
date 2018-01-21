33d8592d1e5c3c087e2d59f220fbdf64de11c6ba
Jon Lauridsen <mail@jonlauridsen.com>
2018-01-21 12:51:14 +0000

    Add catch to server.js startup

--- server.js
+++ server.js
@@ -11,20 +11,26 @@ const app = next({dir: '.', dev: ISDEV})
 const handle = app.getRequestHandler()
 const routes = getRoutes()
 
-app.prepare().then(() => {
-  const server = express()
-  server.get('*', (req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const {pathname, query = {}} = parsedUrl
-    const route = routes[pathname]
-    if (route) {
-      return app.render(req, res, route.page, route.query)
-    }
-    return handle(req, res)
-  })
+app.prepare()
+  .then(() => {
+    const server = express()
+    server.get('*', (req, res) => {
+      const parsedUrl = parse(req.url, true)
+      const {pathname, query = {}} = parsedUrl
+      const route = routes[pathname]
+      if (route) {
+        return app.render(req, res, route.page, route.query)
+      }
+      return handle(req, res)
+    })
 
-  server.listen(PORT, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${PORT}`)
+    server.listen(PORT, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${PORT}`)
+    })
+
   })
-})
+  .catch(err => {
+    console.log('An error occurred, unable to start the server')
+    console.log(err)
+  })
