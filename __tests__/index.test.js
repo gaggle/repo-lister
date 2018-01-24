fd9bac353168ce4eeb5fce9dc02a4cdd7cdafb35
Jon Lauridsen <mail@jonlauridsen.com>
2018-01-24 00:18:08 +0000

    Fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,27 @@
 /* eslint-env jest */
 
-import { shallow } from 'enzyme'
+import MockDate from 'mockdate'
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 
 import App from '../pages/index.js'
 
-import MockDate from 'mockdate'
 
 describe('With Enzyme', () => {
   it('App shows "List of posts" heading', () => {
     const app = shallow(<App posts={[{id: 1}]}/>)
 
-    expect(app.find('h1').text()).toEqual('List of posts')
+    expect(app.find('h2').length).toEqual(2)
   })
 })
 
 describe('With Snapshot Testing', () => {
   it('App matches snapshot', () => {
     MockDate.set('1/1/2000')
-    const component = renderer.create(<App posts={[{id: 2}]}/>)
+    const component = renderer.create(<App posts={[{id: 1}]}/>)
     const tree = component.toJSON()
+
     expect(tree).toMatchSnapshot()
   })
 
--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export default class extends Component {
         </Head>
 
         <Navbar light className="navbar navbar-expand-md pt-3 pb-3">
-          <Link prefetch href="/">
+          <Link href="/">
             <NavbarBrand href="/">
               <span className="icon ion-md-home mr-1"/>Home
             </NavbarBrand>
