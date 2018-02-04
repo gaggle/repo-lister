6c5a7e931c5a757f56e2473074e0f1773475ab88
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-04 11:55:15 +0000

    Disable babel-loader caching into node_modules

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const getRoutes = require('./lib/routes')
 module.exports = {
   exportPathMap: getRoutes,
   webpack: (config, {dev}) => {
+
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+
     config.module.rules.push(
       {
         test: /\.(css|scss)/,
@@ -21,6 +29,7 @@ module.exports = {
         loader: 'babel-loader!raw-loader!sass-loader'
       }
     )
+
     return config
   }
 }
