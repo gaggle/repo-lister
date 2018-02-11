64412c331b88498485da35f9d317d401beefb193
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-11 13:22:42 +0000

    Trivial

--- pages/_document.js
+++ pages/_document.js
@@ -2,13 +2,13 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import flush from 'styled-jsx/server'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage()
+  static getInitialProps ({renderPage}) {
+    const {html, head, errorHtml, chunks} = renderPage()
     const styles = flush()
-    return { html, head, errorHtml, chunks, styles }
+    return {html, head, errorHtml, chunks, styles}
   }
 
-  render() {
+  render () {
     /**
      * Here we use _document.js to add a "lang" propery to the HTML object if
      * one is set on the page.
@@ -19,8 +19,8 @@ export default class MyDocument extends Document {
       </Head>
       <body>
       {this.props.customValue}
-      <Main />
-      <NextScript />
+      <Main/>
+      <NextScript/>
       </body>
       </html>
     )
--- pages/_error.js
+++ pages/_error.js
@@ -10,19 +10,19 @@ import Styles from '../css/index.scss'
 
 export default class Error extends React.Component {
 
-  static propTypes() {
+  static propTypes () {
     return {
       errorCode: React.PropTypes.number.isRequired,
       url: React.PropTypes.string.isRequired
     }
   }
 
-  static getInitialProps({res, xhr}) {
+  static getInitialProps ({res, xhr}) {
     const errorCode = res ? res.statusCode : (xhr ? xhr.status : null)
     return {errorCode}
   }
 
-  render() {
+  render () {
     var response
     switch (this.props.errorCode) {
       case 200: // Also display a 404 if someone requests /_error explicitly
@@ -34,7 +34,11 @@ export default class Error extends React.Component {
             </Head>
             <Container className="pt-5 text-center">
               <h1 className="display-4">Page Not Found</h1>
-              <p>The page <strong>{ this.props.url.pathname }</strong> does not exist.</p>
+              <p>
+                {'The page '}
+                <strong>{this.props.url.pathname}</strong>
+                {' does not exist.'}
+              </p>
               <p><Link href="/"><a>Home</a></Link></p>
             </Container>
           </div>
@@ -60,16 +64,17 @@ export default class Error extends React.Component {
               <style dangerouslySetInnerHTML={{__html: Styles}}/>
             </Head>
             <Container className="pt-5 text-center">
-              <h1 className="display-4">HTTP { this.props.errorCode } Error</h1>
+              <h1 className="display-4">HTTP {this.props.errorCode} Error</h1>
               <p>
-                An <strong>HTTP { this.props.errorCode }</strong> error occurred while
-                trying to access <strong>{ this.props.url.pathname }</strong>
+                {'An '}
+                <strong>HTTP {this.props.errorCode}</strong>
+                {' error occurred while trying to access '}
+                <strong>{this.props.url.pathname}</strong>
               </p>
             </Container>
           </div>
         )
     }
-
     return response
   }
 
