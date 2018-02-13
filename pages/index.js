861e25a5d2328e6f863e2c1c5324ae647b6aaf8d
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-13 23:21:03 +0000

    Experimental masonry layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import { Container } from 'reactstrap'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardColumns,
+  CardText,
+  CardTitle,
+  Container
+} from 'reactstrap'
 import lorem from 'lorem-ipsum'
 
 import data from '../data.json'
 import Layout from '../components/layout'
-import Post from '../components/post'
 
 export default class extends Component {
   static async getInitialProps () {
@@ -17,13 +24,19 @@ export default class extends Component {
   render () {
     return (
       <Layout {...this.props}>
-        <Container>
-          <h2 className="text-center display-4 mt-5 mb-2">Headline</h2>
-          <div>{this.props.content}</div>
+        <Container fluid={true}>
           <h2 className="text-center display-4 mt-5 mb-2">List of posts</h2>
-          <section>
-            {this.props.posts.map(post => <Post {...post} key={post.id}/>)}
-          </section>
+          <CardColumns column-count="2">
+            {this.props.posts.map(el =>
+              <Card key={el.id}>
+                <CardBody>
+                  <CardTitle tag="h2">{`${el.id} ${el.title}`}</CardTitle>
+                  <CardText>{el.body}</CardText>
+                  <Button className="btn-sm">More info</Button>
+                </CardBody>
+              </Card>
+            )}
+          </CardColumns>
         </Container>
       </Layout>
     )
