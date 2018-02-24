f44af28625e66f92297f67aa9c304c07d48315a3
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-24 13:58:33 +0000

    Add sass compiling to Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = (config, configType) => {
+  // configType has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  // Make whatever fine-grained changes you need
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../')
+  })
+
+  return config
+}
