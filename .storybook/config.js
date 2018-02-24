f8257a3c76b56501cc17c9bb1a7d6dbb168421ab
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-24 14:17:01 +0000

    Trivial, remove low-value comment

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { configure } from '@storybook/react'
 
-// automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/)
 
 function loadStories () {
