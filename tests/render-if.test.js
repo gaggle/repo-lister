a90e5281e7ca6955be0ed5713ec684f3097ec673
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-25 21:04:28 +0000

    Add renderIfElse function

--- tests/render-if.test.js
+++ tests/render-if.test.js
@@ -1,16 +1,32 @@
-import renderIf from '../lib/render-if'
+import renderIf, { renderIfElse } from '../lib/render-if'
 
-describe('render-if', () => {
-  it('calls function if condition is true', (done) => {
+describe('renderIf', () => {
+  it('calls function if condition is true', done => {
     renderIf(true, () => {
       done()
     })
   })
 
-  it('does not call function if condition is false', (done) => {
+  it('does not call function if condition is false', done => {
     renderIf(false, () => {
       done.fail(new Error('Must not be called'))
     })
     done()
+  })
+})
+
+describe('renderIfElse', () => {
+  it('calls function if condition is true', done => {
+    renderIfElse(true, () => {
+      done()
+    })
+  })
+
+  it('calls other function if condition is false', done => {
+    renderIfElse(false, () => {
+      done.fail(new Error('Must not be called'))
+    }, () => {
+      done()
+    })
   })
 })
