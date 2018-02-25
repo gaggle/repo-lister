463cb5c5b92e6a2ad1592945275e3f8577822043
Jon Lauridsen <mail@jonlauridsen.com>
2018-02-25 19:29:45 +0000

    Use RepoCards component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react'
-import {
-  Button,
-  Card,
-  CardBody,
-  CardColumns,
-  CardText,
-  CardTitle,
-  Container
-} from 'reactstrap'
+import { Container } from 'reactstrap'
 import lorem from 'lorem-ipsum'
 
 import data from '../data.json'
 import Layout from '../components/layout'
+import { RepoCards } from '../components/repo-card'
 
 export default class extends Component {
   static async getInitialProps () {
@@ -26,17 +19,7 @@ export default class extends Component {
       <Layout {...this.props}>
         <Container fluid={true}>
           <h2 className="text-center display-4 mt-5 mb-2">List of posts</h2>
-          <CardColumns column-count="2">
-            {this.props.posts.map(el =>
-              <Card key={el.id}>
-                <CardBody>
-                  <CardTitle tag="h2">{`${el.id} ${el.title}`}</CardTitle>
-                  <CardText>{el.body}</CardText>
-                  <Button className="btn-sm">More info</Button>
-                </CardBody>
-              </Card>
-            )}
-          </CardColumns>
+          <RepoCards>{this.props.posts}</RepoCards>
         </Container>
       </Layout>
     )
