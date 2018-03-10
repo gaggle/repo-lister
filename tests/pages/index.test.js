426ec4606cd922cd9a6984f6c71928cbf94d6355
Jon Lauridsen <mail@jonlauridsen.com>
2018-03-10 21:24:56 +0000

    Disable page tests
    Introducing too many changes at too high a pace to keep track of tests, and I *think* I’m seeing a pattern of possibly re-architecting the project around a state-store to pull all the information from, or alternatively reworking component props. Either way not worth going too deep in testing at this stage.

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -1,37 +1,14 @@
-import MockDate from 'mockdate'
-import React from 'react'
 import renderer from 'react-test-renderer'
-import { shallow } from 'enzyme'
 
 import App from '../../pages/index.js'
 
 import realData from '../../data'
 
-const fileData = [{full_name: 'user/name', id: 'id'}]
-
-describe('With Enzyme', () => {
-  it('App shows "List of repos" heading', () => {
-    const app = shallow(<App repos={fileData}/>)
-
-    expect(app.find('h2').length).toEqual(1)
-  })
-})
-
-describe('With Snapshot Testing', () => {
-  beforeEach(() => MockDate.set('1/1/2000'))
-
-  it('App matches snapshot', () => {
-    const app = renderer.create(<App repos={fileData}/>)
-    const tree = app.toJSON()
-
-    expect(tree).toMatchSnapshot()
-  })
-
-  afterEach(() => MockDate.reset())
-})
+const getRepos = (data) => Object
+  .values(data)
 
 describe('With real data', () => {
   it('Renders without errors', () => {
-    renderer.create(<App repos={realData}/>)
+    renderer.create(<App repos={getRepos(realData)}/>)
   })
 })
