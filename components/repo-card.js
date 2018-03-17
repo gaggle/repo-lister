72514e9be48832f2a9fbea5d6957b6f07404c6c7
Jon Lauridsen <mail@jonlauridsen.com>
2018-03-17 10:53:00 +0000

    Simplify repo-card
    Now takes simple props. Although it’s still a bit prop hungry but at least it should be clear how to use it now.
    
    This also gets rid of the legacy “RepoCards” (notice suffix’ed s) component in favour of the new extracted one.

--- components/repo-card.js
+++ components/repo-card.js
@@ -1,78 +1,68 @@
 import Link from 'next/link'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import { Card, CardBody, CardColumns, CardText, CardTitle } from 'reactstrap'
-import { pick } from 'lodash'
+import { Card, CardBody, CardText, CardTitle } from 'reactstrap'
 
-import queries from '../lib/data-query-entries'
 import renderIf from '../lib/render-if'
 
 import RepoTitle from './repo-card-title'
 import { Issues, Language, PullRequests, ReadmeBadge } from './github-components'
 
-export class RepoCards extends Component {
-
-  static propTypes = {
-    repos: PropTypes.array.isRequired,
-  }
-
-  render () {
-    return (
-      <CardColumns>
-        {this.props.repos.map(repo =>
-          <RepoCard key={repo.data.full_name}
-                    readme={repo.readme}
-                    {...repo.data}/>
-        )}
-      </CardColumns>
-    )
-  }
-}
-
 export class RepoCard extends Component {
 
   static propTypes = {
     description: PropTypes.string,
-    full_name: PropTypes.string,
+    hasReadme: PropTypes.bool,
     id: PropTypes.any.isRequired,
+    issues: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     language: PropTypes.string,
-    onClick: PropTypes.func,
+    owner_name: PropTypes.string.isRequired,
+    owner_url: PropTypes.string,
+    pullrequests: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    repo_name: PropTypes.string.isRequired,
+    repo_url: PropTypes.string,
   }
 
   static defaultProps = {
-    description: '',
-    onClick: () => {},
+    hasReadme: false
   }
 
   render () {
     return (
-      <div className={this.constructor.name}>
-        <Card key={this.props.id}>
+      <div className="RepoCard">
+        <Card>
           <CardBody>
-            <CardTitle><RepoTitle {...this.props}/></CardTitle>
-
+            <CardTitle><RepoTitle owner={this.props.owner_name}
+                                  owner_url={this.props.owner_url}
+                                  repo={this.props.repo_name}
+                                  repo_url={this.props.repo_url}/></CardTitle>
             <CardText>{this.props.description}</CardText>
-
             <CardText className="gh-stats">
-              {renderIf(this.props.language, () => <Language {...this.props} />)}
-              <Issues {...this.props} />
-              <PullRequests {...this.props} />
-              {renderIf(this.props.readme, () => <ReadmeBadge/>)}
+              {renderIf(this.props.language, () =>
+                <Language language={this.props.language}/>
+              )}
+              {renderIf(this.props.issues, () =>
+                <Issues count={this.props.issues} url={this.props.issues_url}/>
+              )}
+              {renderIf(this.props.pullrequests, () =>
+                <PullRequests count={this.props.pullrequests} url={this.props.pullrequests_url}/>
+              )}
+              {renderIf(this.props.hasReadme, () => <ReadmeBadge/>)}
             </CardText>
-
             <CardText>
-              <Link href={{pathname: '/repo', query: pick(this.props, queries)}}
-                    as={`/repo/${this.props.full_name}`}>
+              <Link href={{pathname: '/repo', query: {id: this.props.id}}}
+                    as={`/repo/${this.props.id}`}>
                 <a>Details</a>
               </Link>
             </CardText>
           </CardBody>
+
         </Card>
-        <style jsx="true">{`
-          .${this.constructor.name} :global(.gh-stats > *) {
-            margin: 1px 8px 1px 0;
-          }
-        `}</style>
+        <style jsx>{`
+        .RepoCard :global(.gh-stats > *) {
+          margin: 1px 8px 1px 0;
+        }
+      `}</style>
       </div>
     )
   }
