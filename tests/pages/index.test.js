8f616548067491f3ddf37ea3ec56d05118cbd75b
Jon Lauridsen <mail@jonlauridsen.com>
2018-03-17 13:38:27 +0000

    Update pages to use store and start data polling

--- tests/pages/index.test.js
+++ tests/pages/index.test.js
@@ -2,13 +2,8 @@ import renderer from 'react-test-renderer'
 
 import App from '../../pages/index.js'
 
-import realData from '../../data'
-
-const getRepos = (data) => Object
-  .values(data)
-
 describe('With real data', () => {
   it('Renders without errors', () => {
-    renderer.create(<App repos={getRepos(realData)}/>)
+    renderer.create(<App/>)
   })
 })
