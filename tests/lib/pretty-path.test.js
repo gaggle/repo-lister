acfcb53121101a3e4e2084b4b3d63603363d4a8a
Jon Lauridsen <mail@jonlauridsen.com>
2018-04-07 16:02:29 +0100

    Add prettifyPath module

---
+++ tests/lib/pretty-path.test.js
@@ -0,0 +1,20 @@
+const {join, resolve} = require('path')
+
+const prettyPath = require('../../lib/pretty-path')
+
+describe('pretty-path', () => {
+  it('should return relative path given a simple subfolder path', () => {
+    const res = prettyPath('foo/bar')
+    expect(res).toEqual('foo/bar')
+  })
+
+  it('should return absolute path given a parent path', () => {
+    const res = prettyPath('../foo')
+    expect(res).toEqual(resolve(join(process.cwd(), '..', 'foo')))
+  })
+
+  it('should always return absolute path if flag is specified', () => {
+    const res = prettyPath('foo', {alwaysAbsolute: true})
+    expect(res).toEqual(join(process.cwd(), 'foo'))
+  })
+})
