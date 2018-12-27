c8dc12b91469ec080b33033b2d13a80f76f47109
Jon Lauridsen <mail@jonlauridsen.com>
2018-12-27 17:52:16 +0100

    Reimplemented pages

---
+++ pages/_app.js
@@ -0,0 +1,53 @@
+import App, { Container } from 'next/app'
+import React from 'react'
+import withRedux from 'next-redux-wrapper'
+import { bindActionCreators } from 'redux'
+import { page } from 'repo-components'
+import { connect, Provider } from 'react-redux'
+
+import { makeStore } from '../store'
+import { dataPoll } from '../store/actions'
+
+import '../lib/styles.sass'
+
+const mapStateToProps = ({data, hasFetchedOnce, requestHistory}) => ({
+  hasInitialized: hasFetchedOnce,
+  repos: data.repos,
+  requestHistory,
+  createdAt: data.createdAt,
+})
+
+const BuildDateLayout = ({createdAt, children}) => <page.Layout buildDate={createdAt}>{children}</page.Layout>
+
+const BuildDateLayoutWithStore = connect(mapStateToProps)(BuildDateLayout)
+
+class AppWithStore extends App {
+  static async getInitialProps ({Component, ctx}) {
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
+    return {pageProps}
+  }
+
+  componentDidMount () {
+    console.debug(`App mounted, isServer=${this.props.isServer}`)
+    this.props.store.dispatch(dataPoll())
+  }
+
+  render () {
+    const {Component, pageProps, store} = this.props
+    return (
+      <Container>
+        <Provider store={store}>
+          <BuildDateLayoutWithStore>
+            <Component {...pageProps} />
+          </BuildDateLayoutWithStore>
+        </Provider>
+      </Container>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  startDataPoll: bindActionCreators(dataPoll, dispatch)
+})
+
+export default withRedux(makeStore, mapStateToProps, mapDispatchToProps)(AppWithStore)
