cc922803154a403bc27526c35d959638eeaeecd3
Jon Lauridsen <mail@jonlauridsen.com>
2018-12-27 17:50:19 +0100

    Delete old files
    Going to re-implement all this library, first lets get rid of the old files

--- stories/clicker.stories.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import Clicker from '../components/clicker'
-
-storiesOf('Clicker', module)
-  .add('Simple', () => <Clicker data='foo'/>)
--- tests/pages/index.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer'
-
-import App from '../../pages/index.js'
-
-describe('With real data', () => {
-  it('Renders without errors', () => {
-    renderer.create(<App/>)
-  })
-})
